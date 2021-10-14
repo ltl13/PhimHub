68cf618f0d290b541059ed0f9c91e114ab621a59
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-10-14 18:08:23 +0700

    [feat: database]: add model customer

---
+++ server/models/Customer.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CustomerSchema = new Schema({
+  customerType: { type: Schema.Types.ObjectId, ref: "customerTypes" },
+  account: { type: Schema.Types.ObjectId, ref: "accounts" },
+  phoneNumber: { type: String, required: true },
+  name: { type: String, required: true },
+  sex: { type: Number, requried: true },
+  dateOfBirth: { type: Date, required: true },
+  status: { type: Boolean, required: true },
+});
+
+module.exports = mongoose.model("customers", CustomerSchema);
--- server/models/Staff.js
+++ server/models/Staff.js
@@ -2,14 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const StaffSchema = new Schema({
-  staffType: { type: Schema.Types.ObjectId, ref: "accounts" },
+  staffType: { type: Schema.Types.ObjectId, ref: "staffTypes" },
+  account: { type: Schema.Types.ObjectId, ref: "accounts" },
   name: { type: String, required: true },
   phoneNumber: { type: String, required: true },
   identityNumber: { type: String, required: true, unique: true },
-  sex: { type: String, required: true },
+  sex: { type: Number, required: true },
   dateOfBirth: { type: Date, required: true },
-  salary: { type: Integer, required: true },
-  status: { type: String, required: true },
+  salary: { type: Number, required: true },
+  status: { type: Boolean, required: true },
 });
 
 module.exports = mongoose.model("staffs", StaffSchema);
