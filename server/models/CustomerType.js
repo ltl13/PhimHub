86331d329f7f1754a5d99294c55b48256deddfcd
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-10-14 19:22:43 +0700

    [refactor: database]: adjust model staffTypes and customerTypes

--- server/models/CustomerType.js
+++ server/models/CustomerType.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const CustomerTypeSchema = new Schema({
   typeName: { type: String, required: true, unique: true },
+  customers: [{ type: Schema.Types.ObjectId, ref: "customers" }],
 });
 
 module.export = mongoose.model("customerTypes", CustomerTypeSchema);
--- server/models/StaffType.js
+++ server/models/StaffType.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const StaffTypeSchema = new Schema({
   position: { type: String, required: true, unique: true },
+  staffs: [{ type: Schema.Types.ObjectId, ref: "staffs" }],
 });
 
 module.export = mongoose.model("staffTypes", StaffTypeSchema);
