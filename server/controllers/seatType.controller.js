86b0760c7a78d31088987269d0fdb27d0b558be9
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-10-31 08:05:13 +0700

    [feat: seat type]: add route update seat type by id

--- server/controllers/seatType.controller.js
+++ server/controllers/seatType.controller.js
@@ -75,7 +75,7 @@ const createSeatType = async (req, res) => {
         message: "This type has existed",
       });
 
-    // Add new position
+    // Add new type
     const newSeatType = new SeatType({
       typeName,
     });
@@ -83,6 +83,54 @@ const createSeatType = async (req, res) => {
     return res.status(201).json({
       success: true,
       message: "New seat type has just been added",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+const updateSeatTypeById = async (req, res) => {
+  try {
+    const { typeName } = req.body;
+
+    // Check if Seat exists in database
+    const seatType = await SeatType.findById(req.params.id);
+    if (!seatType) {
+      return res.status(404).json({
+        success: false,
+        message: "Seat type not found",
+      });
+    }
+
+    // Check if new type name has existed
+    const checker = await SeatType.findOne({
+      typeName,
+    });
+    console.log(seatType.typeName != typeName);
+    if (checker && seatType.typeName != typeName) {
+      return res.status(400).json({
+        success: false,
+        message: "This seat type has existed",
+      });
+    }
+
+    // Update new type name
+    await SeatType.findByIdAndUpdate(
+      req.params.id,
+      {
+        typeName,
+      },
+      { new: true }
+    ).then(async (result) => await result.save());
+
+    // Updated successfully
+    return res.status(200).json({
+      success: true,
+      message: "Seat type has been updated",
     });
   } catch (error) {
     console.log(error);
