5afc8b51565f12f5372f270fba493eda36c64c7c
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-11-11 18:49:44 +0700

    [feat: seat]: update

--- server/controllers/seat.controller.js
+++ server/controllers/seat.controller.js
@@ -4,7 +4,9 @@ const { confirmAccess } = require("../shared/functions");
 const getAllSeats = async (req, res) => {
   // Passed
   try {
-    const allSeats = await Seat.find({ status: { $ne: 0 } }).populate({
+    const allSeats = await Seat.find({
+      status: { $ne: 0 },
+    }).populate({
       path: "seatType",
       select: "typeName",
     });
@@ -54,15 +56,7 @@ const getSeatById = async (req, res) => {
 
 const createSeat = async (req, res) => {
   try {
-    const {
-      code,
-
-      status,
-
-      seatType,
-
-      room,
-    } = req.body;
+    const { code, status, seatType, room } = req.body;
 
     // Check informantion
     let checker = await Seat.findOne({
@@ -91,6 +85,61 @@ const createSeat = async (req, res) => {
       success: true,
       message: "New seat has been created successfully",
       newSeat,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+const updateSeatById = async (req, res) => {
+  try {
+    const { code, status, seatType, room } = req.body;
+
+    const seat = await Seat.findOne({
+      _id: req.params.id,
+      status: { $ne: 0 },
+    });
+    if (!seat)
+      return res.status(406).json({
+        success: false,
+        message: "Seat not found",
+      });
+
+    let checker = await Seat.findOne({
+      code,
+      room,
+      status: { $ne: 0 },
+    });
+
+    if (checker && seat._id != checker._id)
+      return res.status(400).json({
+        success: false,
+        invalid: "code",
+        message: "Seats code in one room can not be duplicated",
+      });
+
+    const updateSeat = await Seat.findOneAndUpdate(
+      {
+        _id: req.params.id,
+        status: { $ne: 0 },
+      },
+      {
+        code,
+        status,
+        seatType,
+        room,
+      },
+      { new: true }
+    );
+
+    return res.status(201).json({
+      success: true,
+      message: "Seat has been updated successfully",
+      updateSeat,
     });
   } catch (error) {
     console.log(error);
