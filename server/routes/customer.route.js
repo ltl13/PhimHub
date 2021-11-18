34bf9c47bf563858b01c72dda584f1992258e1f0
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-11-18 20:18:09 +0700

    [fix: customer, staff]: change password

--- server/routes/customer.route.js
+++ server/routes/customer.route.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const verifyToken = require("../middlewares/auth.middleware");
+const verifyChangePasswordToken = require("../middlewares/changePassword.middleware");
 const {
   getAllCustomers,
   createCustomer,
   loginCustomer,
-  resetPasswordCustomer,
+  sendChangePasswordTokenCustomer,
+  changePasswordCustomer,
   registerCustomer,
   deleteCustomerById,
   updateCustomerById,
@@ -18,7 +20,12 @@ router.get("/get/:id", verifyToken, getCustomerById);
 router.post("/create", verifyToken, createCustomer);
 router.post("/register", registerCustomer);
 router.put("/login", loginCustomer);
-router.put("/reset-password", resetPasswordCustomer);
+router.get("/send-token", sendChangePasswordTokenCustomer);
+router.put(
+  "/change-password",
+  verifyChangePasswordToken,
+  changePasswordCustomer
+);
 router.put("/update/:id", verifyToken, updateCustomerById);
 router.delete("/delete/:id", verifyToken, deleteCustomerById);
 
